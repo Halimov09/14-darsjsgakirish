e9cd38c523bac4cb84d32ac0ca7a0a2e40a92eaf
Sunnatulloh <100553464+Halimov09@users.noreply.github.com>
2025-04-29 23:23:27 +0500

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -2,8 +2,3 @@ const oval = document.querySelector('.switch_all');
 const oval2 = document.querySelector('.switch');
 const body = document.querySelector('body');
 
-oval.addEventListener('click', function() {
-    oval2.classList.toggle('switch2');
-    body.classList.toggle('dark');
-   
-});
